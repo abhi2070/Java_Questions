47414ab6bf9956d42ac193f90cfe742adbda2ba6
Abhijeet singh <97785144+abhi2070@users.noreply.github.com>
2023-11-28 15:47:59 +0530

    WithFriends.java

--- Basic_Question/WithFriends.java
+++ Basic_Question/WithFriends.java
@@ -1,4 +1,5 @@
-package KKPracticeSet.ConditionalLoop03;
+//Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count 
+//the number of days he can go out in the month of August.
 
 import java.util.Scanner;
 public class WithFriends {
