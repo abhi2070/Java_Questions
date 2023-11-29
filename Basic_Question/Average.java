f8cefc4a237450416c283c6750c10a5645d7ad87
Abhijeet singh <97785144+abhi2070@users.noreply.github.com>
2023-11-29 23:05:25 +0530

    Average.java

--- Basic_Question/Average.java
+++ Basic_Question/Average.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         //Calculate Average Of N Numbers
         System.out.println("0 will act as exit.");
         int x=0;
