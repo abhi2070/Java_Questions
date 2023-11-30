2baca3fb74228a81f1b40ad752de0c2dfee27cb2
Abhijeet singh <97785144+abhi2070@users.noreply.github.com>
2023-11-30 11:35:31 +0530

    Add files via upload

---
+++ Basic_Question/ReverseNumber.java
@@ -0,0 +1,16 @@
+package KKPracticeSet.FlowOfProgram01;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int n=23597;
+        
+        int result=0;
+        while(n != 0){
+            int rem=n%10;
+            result=result*10+rem;
+            n /=10;
+        }
+        System.out.println(result);
+    }
+    
+}
