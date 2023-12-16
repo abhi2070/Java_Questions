3b77d225794b6503a31d9189f5fb39d66f45b699
Abhijeet singh <97785144+abhi2070@users.noreply.github.com>
2023-12-16 20:32:29 +0530

    TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,5 +1,3 @@
-
-package KKPracticeSet.Arrays;
 //https://leetcode.com/problems/two-sum/description/
 /**Given an array of integers nums and an integer target, return indices of the 
     two numbers such that they add up to target.
