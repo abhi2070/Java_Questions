a9cba3f86e9c98f09bf15cd7cce29619aee39773
Abhijeet singh <97785144+abhi2070@users.noreply.github.com>
2023-12-16 20:31:09 +0530

    FindNUniqueIntegersSumuptoZero.java

--- Arrays/FindNUniqueIntegersSumuptoZero.java
+++ Arrays/FindNUniqueIntegersSumuptoZero.java
@@ -1,4 +1,3 @@
-package KKPracticeSet.Arrays;
 //https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/description/
 /**
  *Given an integer n, return any array containing n unique integers such that they add up to 0.
